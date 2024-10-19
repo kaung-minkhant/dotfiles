2935d2b1a64d623c78c94f159a38d07694fbc0d2
kaung-minkhant <kaungminkhant.official@gmail.com>
2024-10-19 12:58:54 +0700

    moved home-manager to dotfiles

---
+++ home-manager/home.nix
@@ -0,0 +1,238 @@
+{ config, pkgs, ... }:
+let
+  username = "kaung-min-khant";
+  homeDirectory = "/home/${username}";
+  relativeHomeDirectory = ../..;
+
+  dotfilesGit = pkgs.fetchgit {
+    url = "https://github.com/kaung-minkhant/dotfiles.git";
+    hash = "sha256-xEeqvAjDHqo+BKAtlhaIHpGHYVZvTxnbLQX5lG8VOJg="; 
+  };
+
+  dotfilesLocal = homeDirectory + "/dotfiles";
+
+  dotfiles = dotfilesLocal;
+in
+{
+  imports = [ ./modules/tmux.nix];
+  home.username = username;
+  home.homeDirectory = homeDirectory;
+
+  home.stateVersion = "24.05";
+
+  home.packages = with pkgs; [
+
+    # editor
+    unstable.neovim
+
+    blesh
+		# version control
+    gh
+
+    # archives
+    zip 
+		xz
+		unzip
+		p7zip
+
+		# utils
+    gcc9 
+    glib
+    moreutils
+    gnumake
+    wl-clipboard-x11
+    wl-clipboard
+    curl
+    neofetch
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+		# productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+    obsidian
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # fonts
+    (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+    # other softwares
+    telegram-desktop
+    discord
+    whatsapp-for-linux
+
+    # other dependencies
+    nodejs_22
+    go
+    python312Packages.virtualenv
+  ] ;
+
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "kaungminkhant.official@gmail.com";
+      userName = "kaung-minkhant";
+      aliases = {
+        co = "checkout";
+        br = "branch";
+        st = "status";
+        cm = "commit";
+      };
+    };
+    vim = {
+      enable = true;
+      settings = {
+        relativenumber = true;
+        number = true;
+        tabstop = 2;
+        shiftwidth = 2;
+        expandtab = true;
+      };
+      extraConfig = ''
+        imap jj <Esc>
+      '';
+    };
+    vscode = {
+      enable = false;
+      extensions = with pkgs.vscode-extensions; [
+        ms-python.python
+        bbenoist.nix
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "sha256-LxFOxkcQNCLotgZe2GKc2aGWeP9Ny1BpD1XcTqB85sI=";
+        }
+      ];
+      userSettings = {
+        "terminal.integrated.defaultProfile.linux" = "null";
+        #"terminal.integrated.shell.linux" = "/run/current-system/sw/bin/bash";
+      };
+    };
+    chromium = {
+      enable = false;
+      package = pkgs.brave;
+      extensions = [
+        {
+          id = "jlmpjdjjbgclbocgajdjefcidcncaied";
+        }
+      ];
+    };
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+    bash.enable = false;
+    starship = {
+      enable = true;
+    };
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    #".screenrc".source = /home/kaung-min-khant/dotfiles/screenrc;
+    #"config/.hello".source = dotfiles/hello;
+    "sample" = {
+      source = dotfiles + "/sample";
+      target = "/.config/.sample";
+      enable = true;
+    };
+    "starship" = {
+      source = dotfiles + "/starship.toml";
+      target = "/.config/starship.toml";
+      enable = true;
+    };
+    "nvim" = {
+      source = dotfiles + "/nvim";
+      target = "/.config/nvim";
+      recursive = true;
+      enable = true;
+    };
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/kaung-min-khant/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+		HELLO = "hello kaung";
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ home-manager/modules/tmux.nix
@@ -0,0 +1,94 @@
+{ pkgs, ... }:
+{
+  programs.tmux = {
+    enable = true;
+    prefix = "M-j";
+    baseIndex = 1;
+    clock24 = false;
+    disableConfirmationPrompt = false;
+    escapeTime = 0; # Time in milliseconds for which tmux waits after an escape is input.
+    keyMode = "vi";
+    mouse = false;
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g status-position top
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+          set -g @catppuccin_status_modules_right "directory date_time"
+          set -g @catppuccin_status_modules_left "session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator " "
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+          set -g @catppuccin_directory_text "#{b:pane_current_path}"
+          set -g @catppuccin_date_time_text "%H:%M"
+        '';
+      }
+      tmuxPlugins.sensible
+      {
+        plugin = tmuxPlugins.yank;
+        extraConfig = ''
+          set -g @shell_mode 'vi'
+          set -g @yank_selection 'clipboard'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.tmux-thumbs;
+        extraConfig = ''
+          set -g @thumbs-command 'tmux set-buffer -- {} && wl-copy {} && tmux display-message \"Copied {}\"'
+          set -g @thumbs-upcase-command 'tmux set-buffer -- {} && wl-copy {} && tmx paste-buffer && tmux display-message \"Copied {}\"'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.vim-tmux-navigator;
+        extraConfig = ''
+          set -g @vim_navigator_mapping_left "C-Left C-h"
+          set -g @vim_navigator_mapping_right "C-Right C-l"
+          set -g @vim_navigator_mapping_up "C-k"
+          set -g @vim_navigator_mapping_down "C-j"
+          set -g @vim_navigator_mapping_down "C-j"
+          set -g @vim_navigator_mapping_prev ""
+        '';
+      }
+    ];
+    sensibleOnTop = false;
+    shell = null;
+    terminal = "screen-256color";
+    tmuxinator.enable = false;
+    newSession = false; # Automatically spawn a session if trying to attach and none are running.
+    extraConfig = ''
+      unbind-key -T copy-mode-vi Space
+      unbind-key -T copy-mode-vi 'v'
+      unbind-key '"'
+      unbind-key %
+
+      bind H previous-window
+      bind L next-window
+      bind r command-prompt "rename-window %%"
+      bind | split-window
+      bind s split-window -v -c "#{pane_current_path}"
+      bind v split-window -h -c "#{pane_current_path}"
+      set -g display-time 2000
+      set -g renumber-windows on
+      set -g set-clipboard on
+      bind -T copy-mode-vi v send -X begin-selection
+      set -g pane-active-border-style 'fg=magenta,bg=default'
+      set -g pane-border-style 'fg=brightblack,bg=default'
+    '';
+  };
+}
